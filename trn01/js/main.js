0ceb849466e981033cf52395f0d87b83f0d4825c
leeChangHun <87564335+sipossa@users.noreply.github.com>
2022-05-03 14:46:45 +0900

    Add files via upload

---
+++ trn01/js/main.js
@@ -0,0 +1,13 @@
+// window.addEventListener('DOMContentLoaded', function () {
+//     document.querySelector('.header').style.display = 'none';
+// })
+
+$(function () {
+    $('.main_slider').slick({
+        arrows: false,
+        autoplay: true,
+        autoplaySpeed: 1000,
+        vertical: true,
+        dots: true,
+    });
+})
